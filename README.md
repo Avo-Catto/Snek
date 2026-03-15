bed3ed501728e7a3fbba01ec97aeca75d33d3480
Avo-Catto <lf07@web.de>
2026-03-15 16:53:31 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Installation
 
-> Die Dokumentation ist nur fuer Linux gueltig. Unter Windows kann es leicht variieren.
+> Die Dokumentation ist nur für Linux gültig. Unter Windows kann es leicht variieren.
 
 Zuerst muss eine virtuelle Umgebung erstellt werden:
 ```
@@ -12,14 +12,14 @@ Dann aktiviert man die virtuelle Umgebung:
 source .venv/bin/activate
 ``` 
 
-Danach kann man die Abhaengigkeiten installieren
+Danach kann man die Abhängigkeiten installieren
 ```
 pip install -r requirements.txt
 ```
 
 # Spiel starten
 
-Einfach die `main.py` in virtueller Umgebung ausfuehren:
+Einfach die `main.py` in virtueller Umgebung ausführen:
 ```
 python3 main.py
 ```
@@ -30,7 +30,85 @@ oder wenn der Pfad nicht korrekt aktualisiert wurde mit:
 
 # Anleitung
 
+Als eine Schlange, die stark auf ihre Obst-Ernährung achtet, 
+navigiert man durch verschiedene Level und sammelt dabei Äpfel oder Kirschen ein, 
+die die Schlange direkt auf dem Weg verzehrt.
 
+Äpfel und Kirschen erscheinen beide zufaellig auf der Map.
+Jeder Apfel und jede Kirsche lassen die Snake allerdings um eins länger werden, 
+wobei Äpfel einen und Kirschen zwei Punkte geben. 
+
+Da Kirschen zwei Punkte wert sind, sind sie besonders und erscheinen meist nur für ein paar Sekunden, 
+bevor sie wieder für mehrere Sekunden verschwinden.
+
+Ziel ist es in jedem Level genug Punkte zu sammeln, 
+um ins jeweilige nächste Level aufzusteigen und schlussendlich das Spiel zu gewinnen.
+Dabei darf man nicht mit dem Rand des Spielfeldes, Baumstämmen oder sich selbst kollidieren.
+
+## Steuerung
+
+### In Menues
+
+- **Start:** Linksklick auf den "Spielen"-Button um fortzufahren
+- **Verloren:** Linksklick zum neustarten
+- **Gewonnen:** Linksklick zum neustarten
+- **Level-Up:** kurz warten oder Linksklick um fortzufahren
+
+### Im Spiel
+
+Die Snake bewegt sich automatisch in die Richtung, in die sie guckt.
+Die Steuerung in verschiedene Richtungen ist dabei nicht relativ von der Snake ausgehend, 
+sondern als Himmelsrichtungen zu verstehen. Somit kann die Snake auch niemals in die entgegengesetze Richtung gelenkt werden.
+
+- **Tastenbelegung:**
+  - Linke Pfeiltaste um nach links zu lenken
+  - Rechte Pfeiltaste um nach rechts zu lenken
+  - Obere Pfeiltaste um nach oben zu lenken
+  - Untere Pfeiltaste um nach unten zu lenken
+
+## Level
+
+Die Level sind in Textdateien beschrieben und bestehen aus zwei Teilen:
+
+### Map
+
+Sie besteht aus einem 20x20 Feld von Nullen und Einsen, die durch Kommas separiert werden:
+```
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0
+...
+```
+wobei:
+- 0 - Grasflaeche, auf der die Snake sich fortbewegen kann
+- 1 - Baumstamm, mit dem die Snake nicht kollidieren darf
+
+> Man muss beachten, dass die Map im Spiel dann gespiegelt auf der linken Seite liegt, 
+weshalb sich symmetrische Maps am besten anbieten.
+
+### Parameter
+
+Parameter werden durch ein ">" am Zeilenanfang gekennzeichnet. Sie sind optional und definieren bestimmte Eigenschaften des Levels:
+
+- **"speed":** setzt die Geschwindigkeit der Snake
+- **"next":** setzt die zu erreichende Punktzahl
+- **"snake_pos":** setzt die Startposition der Snake
+- **"snake_dir":** setzt die Startrichtung der Snake
+- **"cherries":** aktiviert / deaktiviert Kirschen
+- **"cherry_time":** gibt in Sekunden an für wie lange die Kirsche erscheint
+- **"cherry_out_time":** gibt die Range in Sekunden an für wie lange die Kirsche verschwindet
+
+Alle Parameter mit Standardwerten:
+```
+> speed 10              # Um eins vorwärts alle FPS(60) - 10 = 50 FPS
+> next 10               # 10 Punkte fuer Level-Up
+> snake_pos 10 10       # x = 10; y = 10
+> snake_dir u           # u = up; d = down; l = left; r = right
+> cherries 1            # 1 = aktiviert; 0 = deaktiviert
+> cherry_time 10        # 10 Sekunden
+> cherry_out_time 5 10  # zwischen 5 bis 10 Sekunden
+```
+
+Kommentare in der Leveldatei sind nicht unterstützt.
 
 # Credits
 
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from random import randint
 import pygame
 from pygame import Surface
 
+# window parameters
 CAPTION = "Snek - Every Masterpiece Got Its Cheap Copy"
 FIELD_SIZE = (30, 30)
 BOARD_SIZE = (20, 20)
 SCREEN_SIZE = (FIELD_SIZE[0] * BOARD_SIZE[0], FIELD_SIZE[1] * BOARD_SIZE[1])
 FPS = 60
 
-START_BUTTON = "assets/play_button.png"
-GAME_OVER = "assets/game_over.png"
+# maps & settings
 MAPS = (
     "maps/Level 1.txt",
     "maps/Level 2.txt",
@@ -21,6 +21,9 @@ MAPS = (
 )
 LEVEL_UP_SEC = 3
 
+# load assets
+START_BUTTON = "assets/play_button.png"
+GAME_OVER = "assets/game_over.png"
 ASSETS = {
     0: pygame.image.load("assets/grass.png"),
     1: pygame.image.load("assets/log.png"),
@@ -32,7 +35,7 @@ ASSETS = {
     7: pygame.image.load("assets/tail.png"),
 }
 
-# Initialialyze music & sounds
+# initialialize music & sounds
 pygame.mixer.init()
 pygame.mixer.music.load("sounds/8bit.mp3")
 
@@ -68,6 +71,7 @@ def direction_from_str(dir: str) -> Direction:
 
 
 def counter_direction(dir: Direction) -> Direction:
+    """Return the opposite direction."""
     match dir:
         case Direction.Up:
             return Direction.Down
@@ -132,7 +136,7 @@ class Apple(Block):
         self.previous_coords = coords
 
     def eat(self) -> None:
-        """Hide apple."""
+        """Hide apple & play sound."""
         self.previous_coords = self.coordinates
         self.coordinates = (-1, -1)
         pygame.mixer.Sound.play(SOUNDS["eat"])
@@ -160,12 +164,13 @@ class Cherry(Apple):
         self.cherry_time = time
 
     def eat(self) -> None:
+        """Hide cherry & play sound."""
         self.coordinates = (-1, -1)
-        print("cherry eaten!\n\n")
         pygame.mixer.Sound.play(SOUNDS["eat2"])
         self.life_time = 0
 
     def spawn(self, obstacles):
+        """Spawn the cherry at a random position on the board."""
         self.life_time = FPS * self.cherry_time
         return super().spawn(obstacles)
 
@@ -174,13 +179,21 @@ class Cherry(Apple):
         out_range: tuple[int, int],
         obstacles: list[Callable[[tuple[int, int]], bool]],
     ) -> None:
+        """Update lifecycle of cherry."""
+        # cherry vanishes
         if self.life_time == 0:
             self.coordinates = (-1, -1)
             self.out_time = FPS * randint(*out_range)
+
+        # countdown until respawn
         elif self.out_time >= 0:
             self.out_time -= 1
+
+        # respawn cherry
         elif self.life_time < 0:
             self.spawn(obstacles)
+
+        # degrade cherry
         self.life_time -= 1
 
 
@@ -203,6 +216,7 @@ class DirectionBlock(Block):
         self.__dir = new
 
     def draw(self, surface: Surface) -> None:
+        """Draw block & transform asset depending on directions."""
         asset = ASSETS[self.id]
         fdir, _ = self.direction
         match fdir.value:
@@ -219,6 +233,8 @@ class DirectionBlock(Block):
 
 
 class Head(DirectionBlock):
+    """Head of Snake."""
+
     def __init__(
         self,
         pos: tuple[int, int],
@@ -229,6 +245,8 @@ class Head(DirectionBlock):
 
 
 class Tail(DirectionBlock):
+    """Tail of Snake."""
+
     def __init__(
         self,
         pos: tuple[int, int],
@@ -239,6 +257,8 @@ class Tail(DirectionBlock):
 
 
 class Part(DirectionBlock):
+    """Mid-section of Snake."""
+
     def __init__(
         self,
         pos: tuple[int, int],
@@ -248,8 +268,11 @@ class Part(DirectionBlock):
         super().__init__(id, pos, dir)
 
     def draw(self, surface: Surface) -> None:
+        """Draw mid-section of snake depending on directions."""
         rotation = 0
         fdir, bdir = self.direction
+
+        # if straight
         if fdir == counter_direction(bdir):
             asset = ASSETS[self.id]
             match fdir.value:
@@ -261,7 +284,7 @@ class Part(DirectionBlock):
                     rotation = 90
                 case 7:
                     rotation = -90
-        else:
+        else:  # if curve
             asset = ASSETS[6]
             match fdir.value + bdir.value:
                 case 5:
@@ -299,15 +322,18 @@ class Snake:
         return self.__parts[0].position
 
     def draw(self, surface: Surface) -> None:
+        """Draw entire snake."""
         for part in self.__parts:
             part.draw(surface)
 
     def set_direction(self, dir: Direction) -> None:
+        """Set head direction."""
         _, bdir = self.__parts[0].direction
-        if dir != bdir:
+        if dir != bdir:  # check if counter direction
             self.__parts[0].direction = (dir, bdir)
 
     def grow(self) -> None:
+        """Grow the snake longer & increase score."""
         self.__move = True
         head = self.__parts[0]
         self.__parts.insert(1, Part(head.coordinates, head.direction))
@@ -352,41 +378,38 @@ class Snake:
             return False
 
 
-class UnknownParameter(Exception):
-    def __str__(self) -> str:
-        return ""
-
-
 class Map:
     def __init__(self) -> None:
         self.__map: list[list[Block]] = []
-        self.speed = 0
-        self.cherries = False
+        self.speed = 10
         self.next_after = 10
         self.snake_coord = (10, 10)
         self.snake_dir = Direction.Up
-        self.cherry_time = 4
+        self.cherries = True
+        self.cherry_time = 10
         self.cherry_out_time = (5, 10)
 
     def load_map(self, path: str):
+        """Load the map & update map parameters."""
         try:
             with open(path, "r") as f:
                 temp = []
                 x = 0
                 for line in f.readlines():
+                    # process map parameters
                     if line.startswith(">"):
                         content = line.split()
                         match content[1].lower():
                             case "speed":
                                 self.speed = int(content[2])
-                            case "cherries":
-                                self.cherries = bool(content[2])
                             case "next":
                                 self.next_after = int(content[2])
                             case "snake_pos":
                                 self.snake_coord = (int(content[2]), int(content[3]))
                             case "snake_dir":
                                 self.snake_dir = direction_from_str(content[2])
+                            case "cherries":
+                                self.cherries = bool(content[2])
                             case "cherry_time":
                                 self.cherry_time = int(content[2])
                             case "cherry_out_time":
@@ -399,19 +422,51 @@ class Map:
                                     f"Error: Unknown Parameter in Level File: {content}"
                                 )
                                 exit(1)
+
+                    # construct actual map
                     else:
+                        # prevent invisible map
+                        if x > BOARD_SIZE[0]:
+                            print("x break")
+                            continue
+
+                        # construct map from file
                         temp.append([])
                         y = 0
                         for i in line.split(","):
-                            temp[-1].append(Block(int(i), (x, y)))
+                            # prevent invisible map
+                            if y >= BOARD_SIZE[1]:
+                                print("y break")
+                                break
+
+                            try:  # increase map file robustness
+                                temp[-1].append(Block(int(i.strip()) % 2, (x, y)))
+                            except Exception as e:
+                                print(f"Warning: error when loading map {e}")
+                                temp[-1].append(Block(0, (x, y)))
                             y += 1
+
+                        # complete y-axes if incomplete
+                        if y < BOARD_SIZE[1] - 1:
+                            for _ in range(BOARD_SIZE[1] - 1 - y):
+                                temp[-1].append(Block(0, (x, y)))
+                                y += 1
                         x += 1
+
+                # complete x-axes if incomplete
+                if x < BOARD_SIZE[0] - 1:
+                    for _ in range(BOARD_SIZE[0] - 1 - x):
+                        # temp.append([Block(0, )])
+                        pass
+
                 self.__map = temp
+
         except Exception as e:
             print(f"Failed loading map: {path}\nError: {e}")
             exit(1)
 
     def draw(self, surface: Surface) -> None:
+        """Draw the map."""
         for row in self.__map:
             for block in row:
                 asset = ASSETS[block.id]
@@ -430,6 +485,7 @@ class Map:
             ]
         ):
             return True
+
         # check obstacle
         if self.__map[x][y].id != 0:
             return True
@@ -457,6 +513,7 @@ def start_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
             if event.type == pygame.QUIT:
                 return False
 
+        # draw & handle input
         surface.blit(button, (0, 0))
         mousepos = pygame.mouse.get_pos()
         if button_hitbox.collidepoint(mousepos):
@@ -468,7 +525,7 @@ def start_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
 
 
 def game_over_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
-    """Game over screen that returns False if exited and True if pressed."""
+    """Game over screen that returns False on exit and True if pressed."""
 
     # load assets
     asset = pygame.image.load(GAME_OVER)
@@ -480,6 +537,7 @@ def game_over_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
             if event.type == pygame.QUIT:
                 return False
 
+        # draw & handle input
         surface.blit(image, (0, 0))
         if pygame.mouse.get_pressed()[0]:
             return True
@@ -489,6 +547,7 @@ def game_over_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
 
 
 def level_up_screen(surface: Surface, clock: pygame.time.Clock, level: int) -> bool:
+    """Level-Up screen that returns True when pressed or after time limit and False on exit."""
     try:  # extract name of level
         name = MAPS[level].split("/")[-1].split(".")[0]
     except Exception:
@@ -511,6 +570,7 @@ def level_up_screen(surface: Surface, clock: pygame.time.Clock, level: int) -> b
             if event.type == pygame.QUIT:
                 return False
 
+        # draw & handle input
         surface.blit(text, rect)
         if pygame.mouse.get_pressed()[0]:
             return True
@@ -520,7 +580,10 @@ def level_up_screen(surface: Surface, clock: pygame.time.Clock, level: int) -> b
     return True
 
 
-def you_won_screen(surface: Surface, clock: pygame.time.Clock):
+def you_won_screen(surface: Surface, clock: pygame.time.Clock) -> bool:
+    """Winning screen that returns True on press and False on exit."""
+
+    # render text
     font = pygame.font.Font(None, 64)
     text = font.render("You won I guess -_-", True, (255, 255, 255))
     rect = text.get_rect()
@@ -530,11 +593,12 @@ def you_won_screen(surface: Surface, clock: pygame.time.Clock):
         # handle exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                return False
 
+        # draw & handle input
         surface.blit(text, rect)
         if pygame.mouse.get_pressed()[0]:
-            exit()
+            return True
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -563,7 +627,10 @@ def main():
     while retry:
         # check if won
         if level == len(MAPS):
-            you_won_screen(screen, clock)
+            if not you_won_screen(screen, clock):
+                exit()
+            else:
+                level = 0
 
         # initialize game components
         world_map = Map()
@@ -575,12 +642,12 @@ def main():
         apple = Apple()
         apple.spawn([snake.is_collision, world_map.is_collision])
         cherry = Cherry(world_map.cherry_time)
-        print(world_map.cherries)
         if world_map.cherries:
             cherry.spawn(
                 [snake.is_collision, world_map.is_collision, apple.is_collision]
             )
 
+        # check if level up
         if level_up:
             if not level_up_screen(screen, clock, level):
                 exit()
@@ -611,6 +678,7 @@ def main():
                     if event.key == pygame.K_RIGHT:
                         snake.set_direction(Direction.Right)
 
+            # implement snake speed
             if counter >= FPS - world_map.speed:
                 counter = 0
 
